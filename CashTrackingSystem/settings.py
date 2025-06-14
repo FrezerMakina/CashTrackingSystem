988cf79fd0e01e0b73cb96bac05508141a2db16a
FrezerMakina <104712102+FrezerMakina@users.noreply.github.com>
2025-06-14 07:46:25 +0200

    set Password timeout and edited reset logic

--- CashTrackingSystem/settings.py
+++ CashTrackingSystem/settings.py
@@ -131,6 +131,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'CashTracker.Staff'
+PASSWORD_RESET_TIMEOUT = 300
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
