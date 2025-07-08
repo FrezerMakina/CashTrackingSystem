460bf72de6fbb64ed7240cc4686f91a70564a019
FrezerMakina <104712102+FrezerMakina@users.noreply.github.com>
2025-07-08 18:47:59 +0200

    Update models.py
    added __str__ functions

--- CashTracker/models.py
+++ CashTracker/models.py
@@ -15,6 +15,9 @@ class Staff(AbstractUser):
     )
     role = models.CharField(max_length=25, choices=position)
     
+    def __str__(self):
+        return self.username
+    
 class Requisition(models.Model):
     ID = models.CharField(primary_key=True, max_length=10)
     date = models.DateTimeField(auto_now_add=True, null=False)
@@ -25,6 +28,9 @@ class Requisition(models.Model):
     projectcode = models.CharField(max_length=10)
     requestingdept = models.CharField(max_length=40)
     accountnumber = models.CharField(max_length=20, null=True, default="6565")
+    
+    def __str__(self):
+        return self.ID
     
 class Item(models.Model):
     ID = models.AutoField(primary_key=True)
@@ -38,6 +44,9 @@ class Item(models.Model):
     def save(self, *args, **kwargs):
         self.totalprice = self.unitprice * self.quantity
         super().save(*args, **kwargs)
+        
+    def __str__(self):
+        return f'{self.itemname} for requisition number {self.requisitionid}'
     
 class Voucher(models.Model):
     ID = models.AutoField(primary_key=True)
@@ -46,8 +55,15 @@ class Voucher(models.Model):
     purpose = models.CharField(max_length=200, null=False)
     date = models.DateTimeField(auto_now_add=True, null=False, blank=False)
     
+    
+    def __str__(self):
+        return f'Voucher ID {self.ID}'
+    
 class Retirement(models.Model):
     ID = models.AutoField(primary_key=True)
     requisitionid = models.ForeignKey(Requisition, on_delete=models.CASCADE)
     voucherid = models.ForeignKey(Voucher, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    
+    def __str__(self):
+        return f'Retirement ID {self.ID} for {self.voucherid}'
